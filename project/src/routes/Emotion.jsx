5cc5a8fe9ff7181f06fba691b0a2ce9c87c2f988
Lee Sangdon <88819145+leeideal@users.noreply.github.com>
2022-08-23 01:35:56 +0900

    leeideal-08-23-F0002-setting2

---
+++ project/src/routes/Emotion.jsx
@@ -0,0 +1,7 @@
+function Emotion () {
+    return (
+        <div>감정비우기, 감정나누기 페이지</div>
+    );
+}
+
+export default Emotion
---
+++ project/src/routes/Mission.jsx
@@ -0,0 +1,6 @@
+function Mission () {
+    return (
+        <div>일일 미션 페이지</div>
+    );
+}
+export default Mission;
