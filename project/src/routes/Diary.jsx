15d0c61740e1856a75b6eaa62db0e4a6a6fb013f
ebokyung <90684277+ebokyung@users.noreply.github.com>
2022-08-24 21:38:41 +0900

    ebokyung-08-24-F0007-질문일기 페이지 헤더 넣기

--- project/src/routes/Diary.jsx
+++ project/src/routes/Diary.jsx
@@ -1,6 +1,32 @@
+import styled from "styled-components";
+import PageHeader from "../components/PageHeader";
+
+const Container = styled.body`
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+`
+
+const Wrapper = styled.section`
+    width : ${props => props.theme.width};
+    max-width: ${props => props.theme.maxWidth};
+    min-height: ${props => props.theme.minHeight};
+`
+
+const pageheader = {
+    title : "질문 일기",
+    body1: "오늘의 나, 한 달 전의 나, 일 년 전의 나는 어땠을까요?",
+    body2 : "책장을 채우고 넘겨가며 나를 되돌아보고 앞으로의 나를 상상해보세요.",
+    width : "125px"
+}
+
 function Diary() {
     return (
-        <div>질문 일기 페이지</div>
+        <Container>
+            <Wrapper>
+                <PageHeader props={pageheader} />
+            </Wrapper>
+        </Container>
     )
 }
 export default Diary;
