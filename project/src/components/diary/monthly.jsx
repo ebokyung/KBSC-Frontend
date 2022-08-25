421ea141271676d3ff446e5080b19e20023a6d3c
ebokyung <90684277+ebokyung@users.noreply.github.com>
2022-08-25 21:35:54 +0900

    ebokyung-08-25-F0007-질문일기페이지 잡동사니

--- project/src/components/diary/monthly.jsx
+++ project/src/components/diary/monthly.jsx
@@ -8,10 +8,11 @@ const Container = styled.section`
 `
 
 function DiaryMonthly () {
+
     return(
     <Container>
-        월별 질문일기
+       {/* booklet 만들어서 들어갈 코드 부분 */}
     </Container>
-    )
+    );
 }
 export default DiaryMonthly;
