c2acea27a04fe6eb9dfa39928e5ee52ff22bf4f9
ebokyung <90684277+ebokyung@users.noreply.github.com>
2022-08-25 12:59:59 +0900

    ebokyung-08-25-F0007-질문일기페이지 섹션나누기

---
+++ project/src/components/diary/monthly.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.section`
+    width: 100%;
+    margin-top: 30px;
+    margin-bottom: 10%;
+    font-family: 'Nanum Myeongjo', serif;
+`
+
+function DiaryMonthly () {
+    return(
+    <Container>
+        월별 질문일기
+    </Container>
+    )
+}
+export default DiaryMonthly;
--- project/src/routes/Diary.jsx
+++ project/src/routes/Diary.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import PageHeader from "../components/PageHeader";
+import DiaryMonthly from '../components/diary/monthly';
+import DiaryDaily from '../components/diary/daily';
 
 const Container = styled.body`
     width: 100vw;
@@ -13,18 +15,27 @@ const Wrapper = styled.section`
     min-height: ${props => props.theme.minHeight};
 `
 
-const pageheader = {
+const pageheader__monthly = {
     title : "질문 일기",
     body1: "오늘의 나, 한 달 전의 나, 일 년 전의 나는 어땠을까요?",
     body2 : "책장을 채우고 넘겨가며 나를 되돌아보고 앞으로의 나를 상상해보세요.",
     width : "125px"
 }
 
+const pageheader__daily = {
+    title : "오늘의 질문",
+    body1: "오늘 하루 중 가장 행복했던 순간은 언제인가요?",
+    width : "155px"
+}
+
 function Diary() {
     return (
         <Container>
             <Wrapper>
-                <PageHeader props={pageheader} />
+                <PageHeader props={pageheader__monthly} />
+                <DiaryMonthly />
+                <PageHeader props={pageheader__daily} />
+                <DiaryDaily />
             </Wrapper>
         </Container>
     )
