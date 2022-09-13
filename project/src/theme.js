7f91759fdd40d394ad988fc56b292f3af8d06e4e
ebokyung <90684277+ebokyung@users.noreply.github.com>
2022-09-13 17:07:26 +0900

    ebokyung-09-13-F0023-테마변경 기능 만드는 중

--- project/src/theme.js
+++ project/src/theme.js
@@ -56,6 +56,248 @@ export const mainTheme ={
     // 채팅방 페이지 색상들
     whoColor : "#55BC98",
     chatBoxBackColor : "#CBE9DE",
+
+    brushColor: "#98D4BF",
 }
 
 // ${props => props.theme.~~};와 같은 방법으로 적용
+
+export const pinkTheme ={
+    // 네브바 색상
+    navBackColor : "#F1A6A0",  // 네브바 배경색
+    navColor1 : "#FBE0D9",   // 네브바 해당 페이지 아닌 색상
+    navColor2 : "#FFFFFF",   // 네브바 해당 페이지
+    navmarginTop : "70px",
+
+
+    // Footer(하단 설명이름) 색상
+    footerBackColor : "#FBE0D9",
+    footerColor : "#FFFFFF",
+
+
+    // 화면 비율
+    width : "86%",
+    maxWidth:"1600px", 
+    minHeight:"90vh",
+
+
+    // pageHead 색상
+    underLineColor : "#FFEFC5",
+
+    // 감정 페이지 색상들
+    emotionBoxBackColor : "rgba(251, 224, 217, 0.15)",
+    emotionBoxBorderColor : "#FBE0D9",
+    emotionItemColor : "#F89790",
+    toggleBtnBackColor : "#F89790",
+    toggleBtnBorderColor : "#F1A6A0",
+    SubmitBtnBackColor : "#F89790",
+    BannerBackColor : "#F89790",
+    itemFooterColor : "#FFC659",
+
+    // 질문일기 페이지 색상들
+    bookPageBtn: "#F1A6A0",
+    bookOuterColor : "#F1A6A0",
+    bookInnerColor : "#FBE0D9",
+    bookBoxGuideTextColor : "#F1A6A0",
+    bookBoxTextColor : "rgba(162, 162, 166, 1)",
+
+    // 미션 페이지 색상들 
+    lineColor : "#F1A6A0",
+    btnColor : "#F1A6A0",
+    
+    // 상담소 페이지 색상들
+    searchBarBorderColor: '#F1A6A0',
+    markerBackColor: '#F1A6A0',
+
+
+    // 로그인, 회원가입 페이지
+    logBtnBackColor : "#F1A6A0",
+
+    // 채팅 페이지 색상들
+    bgColor : "rgba(251, 224, 217, 0.15)",
+    tabTitleColor : "#F1A6A0",
+
+    // 채팅방 페이지 색상들
+    whoColor : "#F1A6A0",
+    chatBoxBackColor : "#FBE0D9",
+}
+
+export const blueTheme ={
+    // 네브바 색상
+    navBackColor : "#92A4E3",  // 네브바 배경색
+    navColor1 : "#C4D1FF",   // 네브바 해당 페이지 아닌 색상
+    navColor2 : "#FFFFFF",   // 네브바 해당 페이지
+    navmarginTop : "70px",
+
+
+    // Footer(하단 설명이름) 색상
+    footerBackColor : "#C4D1FF",
+    footerColor : "#FFFFFF",
+
+
+    // 화면 비율
+    width : "86%",
+    maxWidth:"1600px", 
+    minHeight:"90vh",
+
+
+    // pageHead 색상
+    underLineColor : "#CBE9DE",
+
+    // 감정 페이지 색상들
+    emotionBoxBackColor : "rgba(196, 209, 255, 0.15)",
+    emotionBoxBorderColor : "#C4D1FF",
+    emotionItemColor : "#788FE3",
+    toggleBtnBackColor : "#788FE3",
+    toggleBtnBorderColor : "#92A4E3",
+    SubmitBtnBackColor : "#788FE3",
+    BannerBackColor : "#788FE3",
+    itemFooterColor : "#F1A6A0",
+
+    // 질문일기 페이지 색상들
+    bookPageBtn: "#788FE3",
+    bookOuterColor : "#92A4E3",
+    bookInnerColor : "#C4D1FF",
+    bookBoxGuideTextColor : "#788FE3",
+    bookBoxTextColor : "rgba(162, 162, 166, 1)",
+
+    // 미션 페이지 색상들 
+    lineColor : "#92A4E3",
+    btnColor : "#788FE3",
+    
+    // 상담소 페이지 색상들
+    searchBarBorderColor: '#788FE3',
+    markerBackColor: '#788FE3',
+
+
+    // 로그인, 회원가입 페이지
+    logBtnBackColor : "#788FE3",
+
+    // 채팅 페이지 색상들
+    bgColor : "rgba(196, 209, 255, 0.15)",
+    tabTitleColor : "#788FE3",
+
+    // 채팅방 페이지 색상들
+    whoColor : "#788FE3",
+    chatBoxBackColor : "#C4D1FF",
+}
+
+export const yellowTheme ={
+    // 네브바 색상
+    navBackColor : "#FFC659",  // 네브바 배경색
+    navColor1 : "#FFEFC5",   // 네브바 해당 페이지 아닌 색상
+    navColor2 : "#FFFFFF",   // 네브바 해당 페이지
+    navmarginTop : "70px",
+
+
+    // Footer(하단 설명이름) 색상
+    footerBackColor : "#FFEFC5",
+    footerColor : "#FFFFFF",
+
+
+    // 화면 비율
+    width : "86%",
+    maxWidth:"1600px", 
+    minHeight:"90vh",
+
+
+    // pageHead 색상
+    underLineColor : "#CBE9DE",
+
+    // 감정 페이지 색상들
+    emotionBoxBackColor : "rgba(255, 239, 197, 0.15)",
+    emotionBoxBorderColor : "#FFEFC5",
+    emotionItemColor : "#FFB21D",
+    toggleBtnBackColor : "#FFB21D",
+    toggleBtnBorderColor : "#FFC659",
+    SubmitBtnBackColor : "#FFB21D",
+    BannerBackColor : "#FFB21D",
+    itemFooterColor : "#F1A6A0",
+
+    // 질문일기 페이지 색상들
+    bookPageBtn: "#FFB21D",
+    bookOuterColor : "#FFC659",
+    bookInnerColor : "#FFEFC5",
+    bookBoxGuideTextColor : "#FFB21D",
+    bookBoxTextColor : "rgba(162, 162, 166, 1)",
+
+    // 미션 페이지 색상들 
+    lineColor : "#FFC659",
+    btnColor : "#FFB21D",
+    
+    // 상담소 페이지 색상들
+    searchBarBorderColor: '#FFB21D',
+    markerBackColor: '#FFB21D',
+
+
+    // 로그인, 회원가입 페이지
+    logBtnBackColor : "#FFB21D",
+
+    // 채팅 페이지 색상들
+    bgColor : "rgba(255, 239, 197, 0.15)",
+    tabTitleColor : "#FFB21D",
+
+    // 채팅방 페이지 색상들
+    whoColor : "#FFB21D",
+    chatBoxBackColor : "#FFEFC5",
+}
+
+export const purpleTheme ={
+    // 네브바 색상
+    navBackColor : "#B19EE8",  // 네브바 배경색
+    navColor1 : "#E0D5FF",   // 네브바 해당 페이지 아닌 색상
+    navColor2 : "#FFFFFF",   // 네브바 해당 페이지
+    navmarginTop : "70px",
+
+
+    // Footer(하단 설명이름) 색상
+    footerBackColor : "#E0D5FF",
+    footerColor : "#FFFFFF",
+
+
+    // 화면 비율
+    width : "86%",
+    maxWidth:"1600px", 
+    minHeight:"90vh",
+
+
+    // pageHead 색상
+    underLineColor : "#FBE0D9",
+
+    // 감정 페이지 색상들
+    emotionBoxBackColor : "rgba(224, 213, 255, 0.15)",
+    emotionBoxBorderColor : "#E0D5FF",
+    emotionItemColor : "#917CCF",
+    toggleBtnBackColor : "#917CCF",
+    toggleBtnBorderColor : "#B19EE8",
+    SubmitBtnBackColor : "#917CCF",
+    BannerBackColor : "#917CCF",
+    itemFooterColor : "#F89790",
+
+    // 질문일기 페이지 색상들
+    bookPageBtn: "#917CCF",
+    bookOuterColor : "#B19EE8",
+    bookInnerColor : "#E0D5FF",
+    bookBoxGuideTextColor : "#917CCF",
+    bookBoxTextColor : "rgba(162, 162, 166, 1)",
+
+    // 미션 페이지 색상들 
+    lineColor : "#B19EE8",
+    btnColor : "#917CCF",
+    
+    // 상담소 페이지 색상들
+    searchBarBorderColor: '#917CCF',
+    markerBackColor: '#917CCF',
+
+
+    // 로그인, 회원가입 페이지
+    logBtnBackColor : "#917CCF",
+
+    // 채팅 페이지 색상들
+    bgColor : "rgba(224, 213, 255, 0.15)",
+    tabTitleColor : "#917CCF",
+
+    // 채팅방 페이지 색상들
+    whoColor : "#917CCF",
+    chatBoxBackColor : "#E0D5FF",
+}
